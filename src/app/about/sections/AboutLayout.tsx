d27f9834322e2e119c14f396069673f548cbb7da
Corbin <scalecrm.co@gmail.com>
2026-05-26 12:22:28 +1000

    Update AboutLayout.tsx

--- src/app/about/sections/AboutLayout.tsx
+++ src/app/about/sections/AboutLayout.tsx
@@ -43,7 +43,7 @@ export default function AboutLayout() {
                 </li>
                 <li>
                   <span className={styles.cKey}>Based in</span>
-                  <span className={styles.cVal}>Australia/Canada, AU</span>
+                  <span className={styles.cVal}>Canada, Serving Worldwide, AU</span>
                 </li>
                 <li>
                   <span className={styles.cKey}>Developing</span>
