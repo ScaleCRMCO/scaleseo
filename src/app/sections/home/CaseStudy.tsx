a3f156d5a504a57498a5779be0b58135a86aa726
Corbin <scalecrm.co@gmail.com>
2026-06-07 10:04:34 +1000

    Update CaseStudy.tsx

--- src/app/sections/home/CaseStudy.tsx
+++ src/app/sections/home/CaseStudy.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./CaseStudy.module.css";
+
 export default function CaseStudy() {
   return (
     <section className={`${styles.case} section-dark`} id="work">
